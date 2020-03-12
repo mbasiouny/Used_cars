28645577a3ee375f194b8a42b682c301ae3834ef
Abdelfattah Mohamed <abdelfata7mohammed@gmail.com>
2020-03-12 13:20:43 +0200

    front end

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    //
+    /* function store */
+    public function store()
+    {
+        echo "Welcome to the controller";
+    }
+}
+?>
